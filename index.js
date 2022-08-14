cb8facb4a8a75da20136e26795db69d5bdb3c46d
Tayebiayoub <102774220+Tayebiayoub@users.noreply.github.com>
2022-08-14 23:17:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 15, 2022',
+  birthdate: '8 15, 2022',
   name: 'Hafsa'
 };
 
